2f2aa582d1c9837b6409b4e3380e49e9f2003e72
Navasardian Michael <navasardianmichael@gmail.com>
2020-05-24 18:55:41 +0400

    Updates

---
+++ precache-manifest.889c6dd75ef773d3c60652abb48a330e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab40c76409e0f96b4bb8222c35e527d3",
+    "url": "/colors-slider/index.html"
+  },
+  {
+    "revision": "e6addfa73820dc2490e2",
+    "url": "/colors-slider/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "69b8af4bee5f0b84ffc8",
+    "url": "/colors-slider/static/css/main.393117e9.chunk.css"
+  },
+  {
+    "revision": "e6addfa73820dc2490e2",
+    "url": "/colors-slider/static/js/2.a26334af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/colors-slider/static/js/2.a26334af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b8af4bee5f0b84ffc8",
+    "url": "/colors-slider/static/js/main.38be7f43.chunk.js"
+  },
+  {
+    "revision": "0db056848d4317ad4072",
+    "url": "/colors-slider/static/js/runtime-main.abda71f3.js"
+  }
+]);
